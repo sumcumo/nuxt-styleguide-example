06dd32aa53c4f2786fea7a51d93f4e6787b42ef2
Lasse Diercks <github@lassediercks.de>
2018-05-09 16:58:51 +0200

    feat: initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Example Styleguide
+
+Install via git and delete the .git folder 
+
+`git clone https://github.com/sumcumo/nuxt-styleguide-example.git && rm -rf nuxt-styleguide-example/.git`
+
+
+[Or download this repo as a zip](https://github.com/sumcumo/nuxt-styleguide-example/archive/master.zip)
+
+
---
+++ components/Button.vue
@@ -0,0 +1,61 @@
+<template>
+  <button :class="cssClasses()"><slot></slot></button>
+</template>
+
+<script>
+export default {
+	name: 'MyButton',
+	props: {
+		big: {
+			type: Boolean,
+			default: false
+		}
+	},
+	methods: {
+		cssClasses() {
+			return {
+				[`big-button`]: this.big
+			};
+		}
+	}
+};
+</script>
+
+
+<style lang="scss">
+@import '../design-tokens/colors.scss';
+
+button {
+	font-size: inherit;
+	padding: 0.3em 0.6em;
+	border: none;
+	background-color: $main-color;
+}
+
+.big-button {
+	font-size: 24px;
+	padding: 1em 2em;
+}
+</style>
+
+
+<styleguide-doc>
+### MyButton is used for demo purposes
+</styleguide-doc>
+
+
+<styleguide-states>
+[{
+    name: "Default",
+    content: "I am a button"
+},
+{
+    name: "Big",
+    content: "I am a big button",
+    data: {
+        props: {
+            big: true
+        }
+    }
+}]
+</styleguide-states>
